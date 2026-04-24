090b615d6bcb87e360b9a224b599e5cae2dad133
Sheetal <sheetalbehera771@gmail.com>
2026-04-24 22:24:27 +0530

    fix homepage route

--- server.js
+++ server.js
@@ -69,7 +69,9 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 const PORT = process.env.PORT || 3000;
-
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
 app.listen(PORT, () => {
   console.log("Server running");
 });
